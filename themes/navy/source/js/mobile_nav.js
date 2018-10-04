8b2a6dcc89ba527fea05acb775efb8dc66d93fc9
Andy Tudhope <tuddy0525@gmail.com>
2018-10-04 20:02:08 +0200

    Initial changes

--- themes/navy/source/js/mobile_nav.js
+++ themes/navy/source/js/mobile_nav.js
@@ -1,22 +1,45 @@
-(function() {
-  'use strict';
+$(document).ready(function () {
 
-  var body = document.getElementsByTagName('body')[0];
-  var navToggle = document.getElementById('mobile-nav-toggle');
-  var dimmer = document.getElementById('mobile-nav-dimmer');
-  var CLASS_NAME = 'mobile-nav-on';
-  if (!navToggle) return;
+  var w = Math.max(document.documentElement.clientWidth, window.innerWidth || 0);
 
-  navToggle.addEventListener('click', function(e) {
-    e.preventDefault();
-    e.stopPropagation();
-    body.classList.toggle(CLASS_NAME);
+  $(window).on('resize', function(event) {
+    w = Math.max(document.documentElement.clientWidth, window.innerWidth || 0);
+    setMenu(w);
   });
 
-  dimmer.addEventListener('click', function(e) {
-    if (!body.classList.contains(CLASS_NAME)) return;
+  function setMenu(w){
+    if (w < 1199) {
+      $('.header header').appendTo('.mobile-menu-container .ecosystem');
+      $('.header .secondary-nav').appendTo('.mobile-menu-container .community');
+    }else{
+      $('.mobile-menu-container header').appendTo('.header .header-left');
+      $('.mobile-menu-container .secondary-nav').insertAfter('.header .header-left');
+    };
+  }
 
-    e.preventDefault();
-    body.classList.remove(CLASS_NAME);
+  setMenu(w);
+
+  $('.mobile-menu-trigger, .backdrop, .mobile-menu-container .close').on('click', function (event) {
+    event.preventDefault();
+    $('body').toggleClass('open');
+    $('body').removeClass('open-submenu');
   });
-}());
+
+  $('.mobile-menu-container .mobile-submenu-container-inner .mobile-menu-header .back').on('click', function (event) {
+    event.preventDefault();
+    $('body').removeClass('open-submenu');
+  });
+
+  $('.mobile-menu-container-inner .has-submenu .main-link').each(function (index, element) {
+    $(this).on('click', function (event) {
+      event.preventDefault();
+      var cloneSubMenu = $(this).parent().find('.sub-menu ul').clone(),
+          subMenuTitle = $(this).text();
+      $('.mobile-menu-container .mobile-submenu-container-inner .dropdown nav').empty();
+      $('body').addClass('open-submenu');
+      cloneSubMenu.appendTo('.mobile-menu-container .mobile-submenu-container-inner .dropdown nav');
+      $('.mobile-menu-container .mobile-submenu-container-inner .title').text(subMenuTitle);
+    });
+  });
+  
+});
