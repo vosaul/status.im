aa9bb01b8cfe45c1236af0e94f31e2adfb9525db
Jakub Sokołowski <jakub@status.im>
2018-11-07 17:14:53 +0100

    add templating for nithtly URLs and QR image generation

---
+++ scripts/gen-qr.js
@@ -0,0 +1,23 @@
+const fs  = require('fs')
+const yml = require('js-yaml')
+const qrc = require('qrcode')
+const log = require('fancy-log')
+ 
+const err_handler = (err) => {
+  if (err) { throw err }
+}
+  
+const gen_qr = () => {
+  const qr_path = 'public/nightly/img/'
+  const nightlies_path = 'source/_data/nightlies.yml'
+  
+  var nightlies_file = fs.readFileSync(nightlies_path, 'utf8')
+  var nightlies = yml.safeLoad(nightlies_file)
+  
+  //log(qr_path + 'qr-apk.png (' + nightlies['APK_URL'] + ')')
+  qrc.toFile(qr_path + 'qr-apk.png', nightlies['APK_URL'], {}, err_handler)
+  //log(qr_path + 'qr-ios.png (' + nightlies['IOS_URL'] + ')')
+  qrc.toFile(qr_path + 'qr-ios.png', nightlies['IOS_URL'], {}, err_handler)
+}
+
+module.exports = gen_qr
