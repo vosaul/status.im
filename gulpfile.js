97113cdfc9150aa6ba2caff78c41c150ebc7abcf
Jakub Sokołowski <jakub@status.im>
2019-04-24 17:01:26 +0200

    fix bamboo hr script, update employees
    Signed-off-by: Jakub Sokołowski <jakub@status.im>

--- gulpfile.js
+++ gulpfile.js
@@ -14,7 +14,7 @@ const minify = require('gulp-minify');
 const cleanCSS = require('gulp-clean-css');
 const rename = require("gulp-rename");
 const run = require('gulp-run-command').default;
-// const bamboo = require('./scripts/bamboo-hr');
+const bamboo = require('./scripts/bamboo-hr');
 
 const genqr = require('./scripts/gen-qr');
 const updateBuilds = require('./scripts/update-builds');
@@ -111,9 +111,9 @@ gulp.task('releases', function() {
     return updateBuilds('releases', 'release.json')
 })
 
-// gulp.task('employees', async function() {
-//     return bamboo.saveEmployees('source/_data/employees.yml')
-// })
+gulp.task('employees', async function() {
+    return bamboo.saveEmployees('source/_data/employees.yml')
+})
 
 gulp.task('genqr', function() {
     genqr('nightlies', 'APK',   'public/nightly/img', 'qr-apk.png')
--- scripts/bamboo-hr.js
+++ scripts/bamboo-hr.js
@@ -2,6 +2,7 @@ const fs  = require('fs')
 const yml = require('js-yaml')
 const log = require('fancy-log')
 const err = log.error
+const crypto = require('crypto')
 const request = require('request-promise')
 
 /**
@@ -10,65 +11,74 @@ const request = require('request-promise')
 
 const API_ORG = 'statusim' || process.env.BAMBOO_API_ORG
 const API_TOKEN = process.env.BAMBOO_API_TOKEN
-if (!API_TOKEN) {
-  err('Environment variable BAMBOO_API_TOKEN not set!')
-}else{
-  const API_URL = `https://api.bamboohr.com/api/gateway.php/${API_ORG}/v1`
-  const DEFAULT_FIELDS = [
-    'displayName', 'firstName', 'lastName', 'employeePhoto', 'photoUploaded',
-    'jobTitle', 'division', 'workEmail', 'photoUrl', 'avatarMedium',
-    'customStatusPublicKey', 'customGitHubusername',
-  ].join(',')
-  
-  const DEFAULT_OPTIONS = {
-    auth: { user: API_TOKEN, pass: 'x' },
-    headers: { 'Accept': 'application/json' },
-    json: true,
-  }
-  
-  const getEmployee = async (id, fields = DEFAULT_FIELDS) => {
-    log(`Querying employee: ${id}`)
-    return await request.get({
-      ...DEFAULT_OPTIONS,
-      uri: `${API_URL}/employees/${id}`,
-      qs: { fields },
-    })
-  }
-  
-  const getEmployeesList = async () => {
-    return await request.get({
-      ...DEFAULT_OPTIONS,
-      uri: `${API_URL}/employees/directory`,
-    })
-  }
-  
-  /* some people give username, some URL */
-  const cleanGitHubUsername = (data) => {
-    if (data.customGitHubusername) {
-      let matches = data.customGitHubusername.match(/https:\/\/github.com\/(.*)/)
-      data.customGitHubusername = (matches) ? matches[1] : data.customGitHubusername
-    }
-  }
-  
-  const saveEmployees = async (outFilePath) => {
-    let data = await getEmployeesList()
-    log(`Found active employees: ${data.employees.length}`)
-  
-    let employees = []
-    for (let employee of data.employees) {
-      let rval = await getEmployee(employee.id)
-      cleanGitHubUsername(rval)
-      employees.push(rval)
-    }
-  
-    yamlText = yml.safeDump({ employees }, {lineWidth: 200})
-    let fileContents = (
-      "# Managed via 'employees' gulp task\n" + yamlText
-    )
-    fs.writeFileSync(outFilePath, fileContents)
-    log(`Saved to: ${outFilePath}`)
-  }
-  
-  module.exports = { saveEmployees }
 
+const API_URL = `https://api.bamboohr.com/api/gateway.php/${API_ORG}/v1`
+const PHOTO_URL = `http://${API_ORG}.bamboohr.com/employees/photos`
+const DEFAULT_FIELDS = [
+  'displayName', 'firstName', 'lastName',
+  'jobTitle', 'division', 'workEmail',
+  'customStatusPublicKey', 'customGitHubusername',
+].join(',')
+
+const DEFAULT_OPTIONS = {
+  auth: { user: API_TOKEN, pass: 'x' },
+  headers: { 'Accept': 'application/json' },
+  json: true,
 }
+
+const getEmployee = async (employee, fields = DEFAULT_FIELDS) => {
+  log(`Querying employee: ${employee.workEmail} (ID: ${employee.id})`)
+  return await request.get({
+    ...DEFAULT_OPTIONS,
+    uri: `${API_URL}/employees/${employee.id}`,
+    qs: { fields },
+  })
+}
+
+const getEmployeesList = async () => {
+  return await request.get({
+    ...DEFAULT_OPTIONS,
+    uri: `${API_URL}/employees/directory`,
+  })
+}
+
+/* some people give username, some URL */
+const cleanGitHubUsername = (data) => {
+  if (data.customGitHubusername) {
+    let matches = data.customGitHubusername.match(/https:\/\/github.com\/(.*)/)
+    data.customGitHubusername = (matches) ? matches[1] : data.customGitHubusername
+  }
+}
+
+/* Details: https://www.bamboohr.com/api/documentation/photos.php */
+const addPhotoUrl = (data) => {
+  let email = data.workEmail.trim().toLowerCase()
+  let emailHash = crypto.createHash('md5').update(email).digest("hex")
+  data['photoUrl'] = `${PHOTO_URL}/?h=${emailHash}`
+}
+
+const saveEmployees = async (outFilePath) => {
+  if (!API_TOKEN) {
+    err('Environment variable BAMBOO_API_TOKEN not set!')
+    process.exit(1)
+  }
+  let data = await getEmployeesList()
+  log(`Found active employees: ${data.employees.length}`)
+
+  let employees = []
+  for (let employee of data.employees) {
+    let rval = await getEmployee(employee)
+    cleanGitHubUsername(rval)
+    addPhotoUrl(rval)
+    employees.push(rval)
+  }
+
+  yamlText = yml.safeDump({ employees }, {lineWidth: 200})
+  let fileContents = (
+    "# Managed via 'employees' gulp task\n" + yamlText
+  )
+  fs.writeFileSync(outFilePath, fileContents)
+  log(`Saved to: ${outFilePath}`)
+}
+
+module.exports = { saveEmployees }
